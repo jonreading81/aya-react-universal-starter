d96295a2a16fa9eb07f8f5348f47055f46b63807
Jonathan Reading <jon.reading.email@gmail.com>
2018-10-16 11:55:53 +0100

    housekeeping

--- storybook/stories/exampleBox.js
+++ storybook/stories/exampleBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { ExampleBox } from '../../src/components/presentation';
 
-storiesOf('Button', module)
-  .add('with text', () => (
+storiesOf('Example Box', module)
+  .add('default', () => (
     <ExampleBox />
   ));
